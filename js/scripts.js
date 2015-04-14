db43514a47df6cd1bf203110347d09c95db95b8e
Cynthia Wilhelm <cynthia.k.wilhelm@gmail.com>
2015-04-13 20:19:38 -0700

    Update contact function with new properties

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,18 @@ $(document).ready(function() {
     };
 
   $("#addContact").submit(function(event) {
-    var firstNameForm = $("#firstName").val();
-    var lastNameForm = $("#lastName").val();
-    var addressForm = $("#address").val();
+    var firstNameForm = $("input#firstName").val();
+    var lastNameForm = $("input#lastName").val();
+    var streetForm = $("input#street").val();
+    var cityForm = $("input#city").val();
+    var stateForm = $("input#state").val();
 
     var contact = {
       firstName: firstNameForm,
       lastName: lastNameForm,
-      address: addressForm
+      street: streetForm,
+      city: cityForm,
+      state: stateForm
     };
 
     $("#contact-list").append("<li><span class='contact clickable'>" + contact.firstName + "</span></li>");
@@ -21,7 +25,9 @@ $(document).ready(function() {
     $(".contact").last().click(function() {
       $(".first-name").text(contact.firstName);
       $(".last-name").text(contact.lastName);
-      $(".address").text(contact.address);
+      $(".street").text(contact.street);
+      $(".city").text(contact.city);
+      $(".state").text(contact.state);
       $("#contacts").show();
     });
 
